a77a91cf020f6a9179f6c3a255613397e0d66776
Mikednv <35602963+Mikednv@users.noreply.github.com>
2020-11-05 07:14:00 +0100

    Update database.config.js

--- configurazione/database.config.js
+++ configurazione/database.config.js
@@ -3,4 +3,11 @@ module.exports = {
     USER: "b4043876d65978",
     PASSWORD: "9ef48349",
     DB: "heroku_edc6ea90c028774",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
 };
