e00137aae5aee03a29b7116f21251d53ac5a753e
Mikednv <35602963+Mikednv@users.noreply.github.com>
2020-11-05 09:40:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.listen(port, () => console.log('server started on port', port))
 
 var debug = require('debug')('server:server');
 var http = require('http');
-var app = require('./app');
 /**
  * Get port from environment and store in Express.
  */
