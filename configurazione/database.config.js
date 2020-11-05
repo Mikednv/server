b2c522a2193dfd198b7180d808c0a82d8ba19a0f
Mikednv <35602963+Mikednv@users.noreply.github.com>
2020-11-05 04:55:15 +0100

    Update database.config.js

--- configurazione/database.config.js
+++ configurazione/database.config.js
@@ -1,13 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "mdmc",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
+    HOST: "eu-cdbr-west-03.cleardb.net",
+    USER: "b4043876d65978",
+    PASSWORD: "9ef48349",
+    DB: "heroku_edc6ea90c028774",
 };
